4d232591812ec3353b31a2490dabbce3a268511e
meow38154 <jing76371@gmail.com>
2026-09-01 17:42:40 +0900

    save

---
+++ 2DLegend/Assets/01_Scripts/MoneyManager.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace _01_Scripts
+{
+    public class MoneyManager : MonoBehaviour
+    {
+        [SerializeField] private int money;
+        public int Money 
+        { 
+            get => money;
+            set 
+            { 
+                money = value; 
+                moneyDisplayTmp.text = $"{frontText} {money.ToString()}"; 
+            } 
+        }
+        [SerializeField] private TextMeshProUGUI moneyDisplayTmp;
+        [SerializeField] private string frontText;
+
+        public void AddMoney(int v)
+        {
+            Money += v;
+        }
+    }
+}
---
+++ 2DLegend/Assets/01_Scripts/UI/MoneyPlusEvent.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace _01_Scripts.UI
+{
+    public class MoneyPlusEvent : MonoBehaviour
+    {
+        [SerializeField] public MoneyManager moneyManager;
+        
+        public void MoneyPlus()
+        {
+            moneyManager.AddMoney(1);
+        }
+    }
+}
